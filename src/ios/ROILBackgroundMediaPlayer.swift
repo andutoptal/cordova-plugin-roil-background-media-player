fb9d2c9c76fa905be9eefaae5519ab1ff4f9c12d
Andu Fratu <andu@toptal.com>
2019-10-29 19:03:33 +0100

    Cleanup iOS implementation of background player to avoid stutter and to remove webview lockscreen controls

--- src/ios/ROILBackgroundMediaPlayer.swift
+++ src/ios/ROILBackgroundMediaPlayer.swift
@@ -88,7 +88,7 @@ import WebKit
                     self.player?.currentItem?.removeObserver(self, forKeyPath: #keyPath(AVPlayerItem.status))
                     player?.seek(to: CMTime(seconds: position, preferredTimescale: 1), completionHandler: {_ in
                         self.clearNowPlaying()
-                        self.configureAudioSessionAndStartPlay(options: .mixWithOthers)
+                        self.configureAudioSessionAndStartPlay()
                     })
                 }
             case .failed:
@@ -99,35 +99,22 @@ import WebKit
         }
     }
     
-    func configureAudioSessionAndStartPlay(retry: Bool = true, options: AVAudioSession.CategoryOptions = []) {
-        configureAVAudioSession(shouldRetryIfCantSetExclusive: retry, options, executeAfter: {
-            self.player?.rate = self.playbackSpeed
-            
-            self.setUpRemoteTransportControls()
-            self.setUpNowPlaying()
+    func configureAudioSessionAndStartPlay() {
+        do {
+            try self.avSession.setCategory(.playback, mode: .default, options: [.mixWithOthers])
+            try self.avSession.setActive(true)
+
+            if (self.playbackSpeed == 1.0) {
+                self.player?.play()
+            } else {
+                self.player?.rate = self.playbackSpeed
+            }
             
             self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus.ok), callbackId: self.callbackId)
             self.callbackId = ""
-        })
-    }
-
-    func configureAVAudioSession(shouldRetryIfCantSetExclusive retry: Bool = true, _ options: AVAudioSession.CategoryOptions = [], executeAfter after: @escaping () -> Void = {}) {
-        do {
-            try self.avSession.setCategory(.playback, mode: .default, options: options)
-            try self.avSession.setActive(true)
-
-            after()
         } catch {
             print("Failed to activate audio session, error: \(error)")
-            if (retry) {
-                configureAVAudioSession(shouldRetryIfCantSetExclusive: retry, options, executeAfter: after)
-            }
         }
-    }
-
-    func scheduleAVAudioSessionConfigRetry() {
-        self.timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: false, block: {_ in
-        })
     }
     
     func sendErrorResult() {
@@ -135,55 +122,7 @@ import WebKit
         self.callbackId = ""
     }
     
-    func setUpRemoteTransportControls() {
-        let commandCenter = MPRemoteCommandCenter.shared()
-        
-        commandCenter.playCommand.addTarget { [unowned self] event in
-            if self.player?.rate == 0.0 {
-                self.player?.rate = self.playbackSpeed
-                return .success
-            }
-            return .commandFailed
-        }
-        
-        commandCenter.pauseCommand.addTarget { [unowned self] event in
-            if self.player?.rate == self.playbackSpeed {
-                self.player?.pause()
-                return .success
-            }
-            return .commandFailed
-        }
-    }
-    
     func clearNowPlaying() {
-        setNowPlaying([:])
-    }
-    
-    func setUpNowPlaying() {
-        var nowPlayingInfo = [String : Any]()
-
-        if (!title.isEmpty) {
-            nowPlayingInfo[MPMediaItemPropertyTitle] = title
-        }
-        
-        if (!imageUrl.isEmpty) {
-            let imageData: NSData! = NSData(contentsOf: URL(string: imageUrl)!)
-            let image = UIImage(data: imageData as Data)
-            nowPlayingInfo[MPMediaItemPropertyArtwork] =
-                MPMediaItemArtwork(boundsSize: (image?.size)!) { size in
-                    return image!
-            }
-        }
-
-        let playerItem = player?.currentItem
-        nowPlayingInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = playerItem?.currentTime().seconds ?? 0.0
-        nowPlayingInfo[MPMediaItemPropertyPlaybackDuration] = playerItem?.asset.duration.seconds ?? 0.0
-        nowPlayingInfo[MPNowPlayingInfoPropertyPlaybackRate] = player?.rate ?? 0.0
-        
-        setNowPlaying(nowPlayingInfo)
-    }
-        
-    func setNowPlaying(_ nowPlayingInfo: [String : Any]) {
-        MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = [:]
     }
 }
